9d2bebefea6ddee4dd36e4f87a7ce9a73b0f1dfd
André Machado <andrels.machado82@gmail.com>
2024-10-12 17:35:51 -0300

    Added IAM Role to Terraform module

--- terraform/modules/aws-humangov-infrastructure/main.tf
+++ terraform/modules/aws-humangov-infrastructure/main.tf
@@ -1,18 +1,33 @@
 resource "aws_security_group" "state_ec2_sg" {
-  name = "humangov-${var.state_name}-ec2-sg"
+  name        = "humangov-${var.state_name}-ec2-sg"
   description = "Allow trafic on ports 22 and 80"
 
+  
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+  # ingress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   security_groups = ["<YOUR_CLOUD9_SECGROUP>"]
+  # }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -24,11 +39,12 @@ resource "aws_security_group" "state_ec2_sg" {
 }
 
 resource "aws_instance" "state_ec2" {
-  ami = "ami-007855ac798b5175e"
-  instance_type = "t2.micro"
-  key_name = "humangov-ec2-key"
+  ami                    = "ami-007855ac798b5175e"
+  instance_type          = "t2.micro"
+  key_name               = "humangov-ec2-key"
   vpc_security_group_ids = [aws_security_group.state_ec2_sg.id]
-  subnet_id = "subnet-0351f706a21533540"
+  subnet_id              = "subnet-0351f706a21533540"
+  iam_instance_profile = aws_iam_instance_profile.s3_dynamodb_full_access_instance_profile.name
 
   tags = {
     Name = "humangov-${var.state_name}"
@@ -36,24 +52,24 @@ resource "aws_instance" "state_ec2" {
 }
 
 resource "aws_dynamodb_table" "state_dynamodb" {
-  name = "humangov-${var.state_name}-dynamodb"
+  name         = "humangov-${var.state_name}-dynamodb"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "id"
+  hash_key     = "LockID"
 
   attribute {
-    name = "id"
+    name = "LockID"
     type = "S"
   }
-  
+
   tags = {
     Name = "humagov-${var.state_name}"
   }
 }
 
 resource "random_string" "baucket_suffix" {
-  length = 3
+  length  = 3
   special = false
-  upper = false
+  upper   = false
 }
 
 resource "aws_s3_bucket" "state_s3" {
@@ -63,5 +79,49 @@ resource "aws_s3_bucket" "state_s3" {
     Name = "humangov-${var.state_name}"
   }
 }
+
+resource "aws_iam_role" "s3_dynamodb_full_access_role" {
+  name = "humangov-${var.state_name}-s3_dynamodb_full_access_role"
+
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+
+  tags = {
+    Name : "humangov-${var.state_name}"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access_role_policy_attachment" {
+  role       = aws_iam_role.s3_dynamodb_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_full_access_role_policy_attachment" {
+  role       = aws_iam_role.s3_dynamodb_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_instance_profile" "s3_dynamodb_full_access_instance_profile" {
+  name = "humangov-${var.state_name}-s3_dynamodb_full_access_instance_profile"
+  role = aws_iam_role.s3_dynamodb_full_access_role.name
+
+  tags = {
+    Name = "humangov-${var.state_name}"
+  }
+}
+
 
 
